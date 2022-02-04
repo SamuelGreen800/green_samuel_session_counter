fedc100a8eb5acb680a29a8945cf724932b5b0ad
Samuel Green <samuelgreen800@tutanota.com>
2022-02-04 15:35:53 -0500

    final

--- server.py
+++ server.py
@@ -10,12 +10,19 @@ def index():
     if 'count' not in session:
         session['count'] = 0
         session['count'] += 1
+    if 'count1' not in session:
+        session['count1'] = 0
+        session['count1'] +=1
+    if 'numb' not in session:
+        session ['numb'] = 0
     
     return render_template('index.html')
 
 @app.route('/increment')
 def increment():
     session['count'] += 2
+    session['count1'] +=1
+    session['numb'] += 2
     return redirect('/')
 
 @app.route('/reset')
@@ -25,7 +32,10 @@ def reset():
 
 @app.route('/sensei', methods=['POST'])
 def sensei():
+    
     session['count'] += int(request.form['numb'])
+    session['count1'] += 1
+    session['numb'] += int(request.form['numb'])
     
     return redirect('/')
 
